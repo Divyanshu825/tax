4f5b25167f23cb24e75e1e16ac2581af2b943fd1
janvipatidar <patidarjanvi275@gmail.com>
2024-07-15 14:06:40 +0530

    First commit

---
+++ src/page/About copy/AboutList.jsx
@@ -0,0 +1,22 @@
+import pic1 from "../../Images/spice.jpg"
+import pic2 from "../../Images/spice.jpg"
+
+export const Ab_About = [
+    {
+        title: "Mobility",
+        img: pic2,
+        des: "Bring your Android and iOS application concept to life on every mobile platform. BBT turns your best concepts into a real-time mobile application. We build the apps that are both striking and fully functional. Our appreciated application developers create incredible mobile apps for Android and iOS platforms at affordable. Our developers are reputed digital explorers who constantly push their limit to bring something new and unique in today's highly competitive market.",
+        lang: ["React Native", "Java"],
+    },
+
+
+];
+
+export const Ab_desc = [
+    {
+        title: "OUR VISION",
+        img: pic2,
+        des: "To become the first choice of customer for dates, dry fruits, nuts and other precious items in fruits and vegetables along with imported drinks and wines by bringing the freshest and highest quality to maximum number of customers through our own shops across India. Our priority lies with consistently keeping up our customers by serving them the products without compromising the level of quality",
+        lang: ["React Native", "Java"],
+    },
+];
---
+++ src/page/About copy/Component/AbCarousel/AboutImages.jsx
@@ -0,0 +1,48 @@
+// import pic1 from '../../../../Images/Finger Millet.jpg'
+// import pic2 from '../../../../Images/Browntop Millet.jpg'
+// import pic3 from '../../../../Images/Kodo Millet.jpg'
+// import pic4 from '../../../../Images/Browntop Millet.jpg'
+// import pic5 from '../../../../Images/Foxtail Millet.avif'
+
+
+// export const AboutList_images = [
+//     {
+//         pic: pic1,
+//     },
+//     {
+//         pic: pic2,
+//     },
+//     {
+//         pic: pic3,
+//     },
+//     {
+//         pic: pic4,
+//     },
+//     {
+//         pic: pic5,
+//     },
+
+// ]
+
+// import pic1 from "../../../../Images/Pearl Millet.jpg"
+import pic1 from "../../../../Images/pic1.jpg"
+import pic2 from "../../../../Images/pic2.jpg"
+
+export const AboutList_images = [
+    {
+        pic: pic1,
+    },
+    {
+        pic: pic2,
+    },
+    {
+        pic: pic1,
+    },
+    {
+        pic: pic2,
+    },
+    {
+        pic: pic2,
+    },
+
+]
