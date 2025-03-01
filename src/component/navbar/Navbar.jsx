5ed7080c6c84b9b22abb277827a213a8103d68a1
Divyanshu825 <divygupta825@gmail.com>
2025-03-01 20:33:18 +0530

    Leatest Updates

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,4 +1,3 @@
-// src/Navbar.js
 import React, { useState } from 'react';
 import './Navbar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -7,6 +6,7 @@ import Logo from '../../Images/LogoJPG.png';
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const [dropdownOpen, setDropdownOpen] = useState(false);
 
     const toggleNavbar = () => {
         setIsOpen(!isOpen);
@@ -16,13 +16,28 @@ const Navbar = () => {
         <header>
             <div className="navbar">
                 <div className="logo">
-                    <img src={Logo} alt="" />
+                    <img src={Logo} alt="Logo" />
                 </div>
                 <ul className={`links ${isOpen ? 'open' : ''}`}>
                     <li><a href="/">Home</a></li>
                     <li><a href="/about">About</a></li>
                     <li><a href="/service">Service</a></li>
-                    <li><a href="/incomeTax">Updates</a></li>
+                    
+                    {/* Updates Dropdown with Unique Class Names */}
+                    <li 
+                        className="updates-dropdown"
+                        onMouseEnter={() => setDropdownOpen(true)}
+                        // onMouseLeave={() => setDropdownOpen(false)}
+                    >
+                        <a href="/incomeTax">Leatest Updates</a>
+                        {dropdownOpen && (
+                            <ul className="updates-menu">
+                                <li><a href="/gst">GST</a></li>
+                                <li><a href="/incomeTax">Income Tax</a></li>
+                            </ul>
+                        )}
+                    </li>
+
                     <li><a href="/contact">Contact</a></li>
                     <li><a href="/Signup">SignUp</a></li>
                     <li><a href="/admin">Admin</a></li>
@@ -31,44 +46,17 @@ const Navbar = () => {
                     <FontAwesomeIcon icon={isOpen ? faXmark : faBars} />
                 </div>
             </div>
+            
             {isOpen && (
                 <div className="dropdown_menu">
                     <a href="/"><li>Home</li></a>
-                    <a href="/about">
-                        <li>
-                            About
-                        </li>
-                    </a>
-                    <a href="/service">
-                        <li>
-                            Service
-                        </li>
-                    </a>
-                    <a href="/client">
-                        <li>
-                            OurClient
-                        </li>
-                    </a>
-                    <a href="/job">
-                        <li>
-                            Jobs
-                        </li>
-                    </a>
-                    <a href="/contact">
-                        <li>
-                            Contact
-                        </li>
-                    </a>
-                    <a href="/images">
-                        <li>
-                            Images
-                        </li>
-                    </a>
-                    <a href="/" className="action_btn">
-                        <li>
-                            Get Started
-                        </li>
-                    </a>
+                    <a href="/about"><li>About</li></a>
+                    <a href="/service"><li>Service</li></a>
+                    <a href="/client"><li>OurClient</li></a>
+                    <a href="/job"><li>Jobs</li></a>
+                    <a href="/contact"><li>Contact</li></a>
+                    <a href="/images"><li>Images</li></a>
+                    <a href="/" className="action_btn"><li>Get Started</li></a>
                 </div>
             )}
         </header>
