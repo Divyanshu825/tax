1fee99cda8264d758a6bee366b783fed6802014b
Janvi <patidarjanvi275@gmail.com>
2025-03-02 22:19:35 +0530

    ProtectedRoute,LoginSignups

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ const App = () => {
       
         <Routes>
           <Route path="/" element={<Home />} />
+          
           <Route path="/about" element={<About />} />
           <Route path="/service" element={<Service />} />
           <Route path="/client" element={<OurClient />} />
